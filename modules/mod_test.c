e0e77e5ec73ee2466dad5e586c5cff542aabc0b5
Michal Novotny <minovotn@redhat.com>
2012-11-07 15:43:56 +0100

    modules: Add test module
    Signed-off-by: Michal Novotny <mignov@gmail.com>

---
+++ modules/mod_test.c
@@ -0,0 +1,29 @@
+#define DEBUG_MOD_TEST
+
+#include "../modules.h"
+
+#ifdef DEBUG_MOD_TEST
+#define DPRINTF(fmt, args...) \
+do { fprintf(stderr, "[cdv/mod_test    ] " fmt , ##args); } while (0)
+#else
+#define DPRINTF(fmt, args...) do {} while(0)
+#endif
+
+void cdvws_process() {
+	char *v = NULL;
+
+	v = variable_get_element_as_string("module_var", NULL);
+	if (v != NULL) {
+		DPRINTF("We have module var set to '%s'\n", v);
+
+		variable_add("module_var", v, TYPE_MODULE, 0, TYPE_STRING);
+	}
+	else {
+		DPRINTF("We don't have module var set, setting to \"unset\"\n");
+
+		variable_add("module_var", "unset", TYPE_MODULE, 0, TYPE_STRING);
+	}
+
+	free(v);
+}
+
